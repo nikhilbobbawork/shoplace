83831165a8b986dd3985d263fb657450130b7739
Nikhil Bobba <nikhilbobbawork@gmail.com>
2026-09-13 19:51:36 -0500

    Initial Commit

---
+++ shoplace-backend/src/main/java/com/shoplace/shoplace_backend/ShoplaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shoplace.shoplace_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoplaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoplaceBackendApplication.class, args);
+	}
+
+}
